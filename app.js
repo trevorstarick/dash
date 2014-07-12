bab6af267db714d6c3bc66edad1a62976091fcc3
Trevor Starick <trevor.starick@gmail.com>
2014-07-12 03:56:34 -0400

    Major redesign.

--- app.js
+++ app.js
@@ -14,10 +14,26 @@ app.use(bodyParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+var blocks = {};
+
+hbs.registerHelper('extend', function(name, context) {
+  var block = blocks[name];
+  if (!block) {
+    block = blocks[name] = [];
+  }
+
+  block.push(context.fn(this));
+});
+
+hbs.registerHelper('block', function(name) {
+  var val = (blocks[name] || []).join('\n');
+
+  blocks[name] = [];
+  return val;
+});
+
 app.get('/', function(req, res) {
-  res.render('layout', {
-    pages: ['home', 'about', 'users']
-  });
+  res.render('index');
 });
 
 app.listen(3000);
